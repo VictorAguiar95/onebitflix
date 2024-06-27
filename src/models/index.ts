5a01d63d5d78656b0a80fdc7552cd9b32f436769
Victor Aguiar <victor.aguiar2504@gmail.com>
2024-06-27 05:48:35 -0300

    Novas Rotas Definidas

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,18 +1,32 @@
 import { Category } from './Category'
 import { Course } from './Course'
 import { Episode } from './Episode'
+import { Favorite } from './Favorite'
 import { User } from './User'
+import { Like } from './Like'
 
 Category.hasMany(Course, { as: 'courses' })
 
 Course.belongsTo(Category)
+Course.belongsToMany(User, { through: Favorite })
+Course.belongsToMany(User, { through: Like })
 Course.hasMany(Episode, { as: 'episodes' })
+Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 
 Episode.belongsTo(Course)
+
+Favorite.belongsTo(Course)
+Favorite.belongsTo(User)
+
+User.belongsToMany(Course, { through: Favorite })
+User.belongsToMany(Course, { through: Like })
+User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
 
 export {
   Category,
   Course,
   Episode,
+  Favorite,
+	Like,
   User
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,32 @@
 import express from 'express'
+import { authController } from './controllers/authController'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/courseController'
+import { episodesController } from './controllers/episodesController'
+import { favoritesController } from './controllers/favoritesController'
+import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
+import { likesController } from './controllers/likesController'
 
 const router = express.Router()
 
-router.get('/categories', categoriesController.index)
-router.get('/categories/:id', categoriesController.show)
+router.post('/auth/register', authController.register)
+router.post('/auth/login', authController.login)
 
-router.get('/courses/featured', coursesController.featured)
+router.get('/categories', ensureAuth, categoriesController.index)
+router.get('/categories/:id', ensureAuth, categoriesController.show)
+
+router.get('/courses/featured', ensureAuth, coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
-router.get('/courses/search', coursesController.search)
-router.get('/courses/:id', coursesController.show)
+router.get('/courses/search', ensureAuth, coursesController.search)
+router.get('/courses/:id', ensureAuth, coursesController.show)
+
+router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
+
+router.get('/favorites', ensureAuth, favoritesController.index)
+router.post('/favorites', ensureAuth, favoritesController.save)
+router.delete('/favorites', ensureAuth, favoritesController.delete)
+
+router.post('/likes', ensureAuth, likesController.save)
+router.delete('/likes', ensureAuth, likesController.delete)
 
 export { router }
